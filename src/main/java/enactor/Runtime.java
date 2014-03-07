1470c1c94d379226c6a954a4b2ec325a91c46bc7
abel-carrion <abcarcol@i3m.upv.es>
2014-03-07 10:24:56 +0100

    Cluster execution working!

--- src/main/java/enactor/Runtime.java
+++ src/main/java/enactor/Runtime.java
@@ -1,5 +1,6 @@
 package enactor;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -34,16 +35,20 @@ public class Runtime {
 		//Commandline for downloading the files
 		for(int i=0; i<s.getStagein().size(); i++){
 			StageIn sgin = s.getStagein().get(i);
-			for(int j=0; j<sgin.getValues().length; j++){
+			for(int j=0; j<sgin.getValues().size(); j++){
+				e = new Execution();
 				e.setPath("wget");
-				e.setArguments(sgin.getValues()[j]);
+				e.setArguments("--no-check-certificate -P ./" + SessionID + " " + sgin.getValues().get(j));
+				s.getExecution().add(e);
 			}
 		}
 		PBS_Connector con = new PBS_Connector(w.queryHost(s),w.queryEnv(s));
+		con.submit(s);
 	}
 	
 	public void run_deploy(Stage s){
 		//TODO: Call deploy infrastructure method of IM
+		
 	}
 	
 	public void run_process(Stage s){
@@ -56,6 +61,9 @@ public class Runtime {
 	
 	public void run(){
 		
+		//ONLY FOR TESTING PURPOSES 
+		run_copy(w.getStages().get(3));
+		// ONLY FOR TESTING PURPOSES
 		int nStages = w.getStages().size();
 		while(nStages!=0){ //there are pending stages to be executed
 			for(int i=0; i<w.getStages().size(); i++){
@@ -72,10 +80,6 @@ public class Runtime {
 						}
 					}
 					if(isEnabled){
-						List<Execution> executions = s.getExecution();
-						for(int k=0; k<executions.size(); k++){
-							//TODO: Execute executions.get(k)
-						}
 						mongo.updateStageStatus(s, Status.RUNNING);
 					}
 				}
